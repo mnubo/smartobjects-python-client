dbd11fcc20f6770afa097e65d0a81c82b7f0c334
Guillaume Paquet <gpaquet@mnubo.com>
2015-12-14 15:16:46 -0500

    Update tests with access token

--- mnubo/tests/test_auth_manager.py
+++ mnubo/tests/test_auth_manager.py
@@ -1,28 +1,30 @@
 from mnubo.api_manager import APIManager
 import requests
-import json
+import datetime
 from requests import Response
 
 from mock import MagicMock
 
+
 def test_auth_maneger_init():
 
     response = Response()
-    response._content = '{"access_token":"CLIENT_ACCESS_TOKEN","token_type":"Bearer","expires_in":3887999}'
+    response._content = '{"access_token":"ACCESS_TOKEN","token_type":"Bearer","expires_in":3887999}'
     requests.post = MagicMock(return_value=response)
     auth = APIManager('CLIENT_ID', 'CLIENT_SECRET', 'HOSTNAME')
 
     requests.post.assert_called_with('HOSTNAME/oauth/token?grant_type=client_credentials', headers={'content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Basic Q0xJRU5UX0lEOkNMSUVOVF9TRUNSRVQ='})
 
-    auth.client_access_token = auth.fetch_client_access_token()
+    auth.access_token = auth.fetch_access_token()
     auth_authorization_header = auth.get_token_authorization_header()
     authorization_header = auth.get_authorization_header()
     api_url = auth.get_api_url()
     auth_url = auth.get_auth_url()
 
-    assert auth.client_access_token == 'CLIENT_ACCESS_TOKEN'
+    assert auth.access_token.token == 'ACCESS_TOKEN'
+    assert auth.access_token.expires_in == datetime.timedelta(seconds=3887999)
     assert auth_authorization_header == {'content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Basic Q0xJRU5UX0lEOkNMSUVOVF9TRUNSRVQ='}
-    assert authorization_header == {'content-type': 'application/json', 'Authorization': 'Bearer CLIENT_ACCESS_TOKEN'}
+    assert authorization_header == {'content-type': 'application/json', 'Authorization': 'Bearer ACCESS_TOKEN'}
     assert api_url == 'HOSTNAME/api/v3/'
     assert auth_url == 'HOSTNAME/oauth/token?grant_type=client_credentials'
 
@@ -30,7 +32,7 @@ def test_auth_maneger_init():
 def test_create_operations():
 
     response = Response()
-    response._content = '{"access_token":"CLIENT_ACCESS_TOKEN","token_type":"Bearer","expires_in":3887999}'
+    response._content = '{"access_token":"ACCESS_TOKEN","token_type":"Bearer","expires_in":3887999}'
     requests.post = MagicMock(return_value=response)
 
     auth = APIManager('CLIENT_ID', 'CLIENT_SECRET', 'HOSTNAME')
