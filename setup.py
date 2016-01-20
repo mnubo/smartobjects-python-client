27280ec3c83f64a976f984dc6fd51e773aa607ac
Guillaume Paquet <gpaquet@mnubo.com>
2016-01-20 13:50:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup
 
 setup(
-    name="mnubo",
+    name="mnubo-sdk",
     version="0.0.1",
     description="Python SDK to access mnubo ingestion APIs",
     author="mnubo",
     author_email="sos@mnubo.com",
     url="https://github.com/mnubo/mnubo-python-sdk",
-    packages=["mnubo"],
+    packages=["mnubo-sdk"],
     install_requires=['requests>=2.5,<3.0'],
-    tests_require=['nose', 'mock']
+    tests_require=['nose', 'mock'],
+    keywords = ['mnubo', 'api', 'sdk', 'iot', 'smartobject']
 )
