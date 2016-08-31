112d8e2d916ee41d5093e007197d8be07d5fcb8d
Manuel Vonthron <mvonthron@mnubo.com>
2016-08-31 11:45:06 -0400

    better package versioning (enables auto versioning from Jenkins)

--- mnubo/__init__.py
+++ mnubo/__init__.py
@@ -2,7 +2,3 @@ from mnubo.mnubo_client import MnuboClient
 from mnubo.mnubo_client import Environments
 from mnubo.api_manager import APIManager
 
-__author__ = "mnubo, inc."
-__email__ = "support@mnubo.com"
-__version__ = "2.0.0"
-__copyright__ = "Copyright (c) 2016 mnubo. All rights reserved."
